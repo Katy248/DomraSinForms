18a130229499211c078c5e858fca529b729d4a0c
Katy248 <petrovanton247@gmail.com>
2023-04-23 15:05:07 +0300

    Add delete command

---
+++ DomraSinForms.Application/Questions/Commands/Delete/DeleteQuestionCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomraSinForms.Application.Interfaces;
+
+namespace DomraSinForms.Application.Questions.Commands.Delete;
+public class DeleteQuestionCommand : IDeleteRequest
+{
+    public string Id { get; set; }
+}
---
+++ DomraSinForms.Application/Questions/Commands/Delete/DeleteQuestionCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Forms.Mvc.Data;
+using MediatR;
+
+namespace DomraSinForms.Application.Questions.Commands.Delete;
+public class DeleteQuestionCommandHandler : IRequestHandler<DeleteQuestionCommand, bool>
+{
+    private readonly ApplicationDbContext _context;
+
+    public DeleteQuestionCommandHandler(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<bool> Handle(DeleteQuestionCommand request, CancellationToken cancellationToken)
+    {
+        var question = await _context.Questions.FindAsync(request.Id, cancellationToken);
+
+        if (question is null)
+            return false;
+
+        _context.Questions.Remove(question);
+        await _context.SaveChangesAsync(cancellationToken);
+        return true;
+    }
+}
