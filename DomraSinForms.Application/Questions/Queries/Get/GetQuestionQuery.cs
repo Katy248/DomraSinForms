fd6057d02b424fa8d59c1bdeb7adf609c363d824
Katy248 <petrovanton247@gmail.com>
2023-04-23 15:27:36 +0300

    Add get query

---
+++ DomraSinForms.Application/Questions/Queries/Get/GetQuestionQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomraSinForms.Application.Interfaces;
+using DomraSinForms.Domain.Models.Questions;
+
+namespace DomraSinForms.Application.Questions.Queries.Get;
+public class GetQuestionQuery : IGetSingleRequest<QuestionBase>
+{
+    public string Id { get; set; }
+}
---
+++ DomraSinForms.Application/Questions/Queries/Get/GetQuestionQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomraSinForms.Domain.Models.Questions;
+using Forms.Mvc.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DomraSinForms.Application.Questions.Queries.Get;
+public class GetQuestionQueryHandler : IRequestHandler<GetQuestionQuery, QuestionBase>
+{
+    private readonly ApplicationDbContext _context;
+
+    public GetQuestionQueryHandler(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<QuestionBase> Handle(GetQuestionQuery request, CancellationToken cancellationToken)
+    {
+        var questionBase = await _context.Questions.FindAsync(request.Id, cancellationToken);
+        switch (questionBase)
+        {
+            case TextQuestion:
+                return await _context.TextQuestions.FirstOrDefaultAsync(q => q.Id == request.Id);
+            case OptionsQuestion:
+                return await _context.OptionsQuestions.Include(q => q.Options).FirstOrDefaultAsync(q => q.Id == request.Id);
+            case NumberQuestion:
+                return await _context.TextQuestions.FirstOrDefaultAsync(q => q.Id == request.Id);
+        }
+
+        return null;
+    }
+}
