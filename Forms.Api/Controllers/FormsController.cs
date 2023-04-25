9c845ce469e3994d2e64c3eb59d47d25f64f21a4
Katy248 <petrovanton247@gmail.com>
2023-04-25 13:16:58 +0300

    Add api project and change database name

---
+++ Forms.Api/Controllers/FormsController.cs
@@ -0,0 +1,17 @@
+﻿using DomraSinForms.Application.Forms.Commands.Create;
+using DomraSinForms.Application.Forms.Commands.Delete;
+using DomraSinForms.Application.Forms.Commands.Update;
+using DomraSinForms.Application.Forms.Queries.Get;
+using DomraSinForms.Application.Forms.Queries.GetList;
+using DomraSinForms.Domain.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forms.Api.Controllers;
+
+public class FormsController : CRUDController<Form, CreateFormCommand, GetFormQuery, GetFormListQuery, UpdateFormCommand, DeleteFormCommand>
+{
+    public FormsController(IMediator mediator) : base(mediator)
+    {
+    }
+}
---
+++ Forms.Api/Controllers/QuestionsController.cs
@@ -0,0 +1,22 @@
+﻿using DomraSinForms.Application.Questions.Commands.Create;
+using DomraSinForms.Application.Questions.Commands.Delete;
+using DomraSinForms.Application.Questions.Commands.Update;
+using DomraSinForms.Application.Questions.Queries.Get;
+using DomraSinForms.Application.Questions.Queries.GetList;
+using DomraSinForms.Domain.Models.Questions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forms.Api.Controllers;
+
+public class QuestionsController : CRUDController<QuestionBase, CreateQuestionCommand, GetQuestionQuery, GetQuestionListQuery, UpdateQuestionCommand, DeleteQuestionCommand>
+{
+    public QuestionsController(IMediator mediator) : base(mediator)
+    {
+    }
+    [NonAction]
+    public override Task<QuestionBase> Create([FromBody] CreateQuestionCommand command)
+    {
+        throw new NotImplementedException();
+    }
+}
