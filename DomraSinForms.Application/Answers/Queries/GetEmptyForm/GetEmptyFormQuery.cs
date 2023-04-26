560cb3c3d3b46c79cc7a1e916d5eb21424f8f1d8
Katy248 <petrovanton247@gmail.com>
2023-04-26 19:41:14 +0300

    Add get empty  form query

---
+++ DomraSinForms.Application/Answers/Queries/GetEmptyForm/GetEmptyFormQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomraSinForms.Application.Answers.Commands.Create;
+using MediatR;
+
+namespace DomraSinForms.Application.Answers.Queries.GetEmptyForm;
+public class GetEmptyFormQuery : IRequest<CreateFormAnswersCommand>
+{
+    public string FormId { get; set; }
+    public string UserId { get; set; }
+}
