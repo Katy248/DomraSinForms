8027a91ed425d13df4d0ba52a5ced963267f4e55
Fores <Fores@DESKTOP-Q9GI0NO>
2023-04-27 00:07:09 +0300

    Add Get command for Answer

---
+++ DomraSinForms.Application/Answers/Queries/GetFormAnswers/GetFormAnswersQuery.cs
@@ -0,0 +1,15 @@
+﻿using DomraSinForms.Application.Interfaces;
+using DomraSinForms.Domain.Models.Answers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomraSinForms.Application.Answers.Queries.GetFormAnswers
+{
+    public class GetFormAnswersQuery: IGetSingleRequest<FormAnswers>
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ DomraSinForms.Application/Answers/Queries/GetFormAnswers/GetFormAnswersQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using DomraSinForms.Domain.Models.Answers;
+using Forms.Mvc.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomraSinForms.Application.Answers.Queries.GetFormAnswers
+{
+    public class GetFormAnswersQueryHandler:IRequestHandler<GetFormAnswersQuery, FormAnswers>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetFormAnswersQueryHandler(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<FormAnswers> Handle(GetFormAnswersQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.FormAnswers.Include(f => f.Answers).FirstOrDefaultAsync(f => f.Id == request.Id, cancellationToken);
+        }
+    }
+}
