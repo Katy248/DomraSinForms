2fd197b8d7a8622dcd57c248c859f40ae21403fc
Katy248 <petrovanton247@gmail.com>
2023-04-27 11:11:22 +0300

    Add get list command for FormAnswers,

---
+++ DomraSinForms.Application/Answers/Queries/GetList/FormAnswersDto.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using DomraSinForms.Application.Mapper;
+using DomraSinForms.Domain.Models.Answers;
+
+namespace DomraSinForms.Application.Answers.Queries.GetList;
+
+public class FormAnswersDto : IMapWith<FormAnswers>
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string CreatorId { get; set; }
+    public List<Answer> Answers { get; set; }
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<FormAnswersDto, FormAnswers>()
+            .ReverseMap();
+    }
+}
---
+++ DomraSinForms.Application/Answers/Queries/GetList/GetFormAnswersListQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DomraSinForms.Application.Mapper;
+using DomraSinForms.Domain.Models.Answers;
+using Forms.Mvc.Data;
+using MediatR;
+
+namespace DomraSinForms.Application.Answers.Queries.GetList;
+public class GetFormAnswersListQueryHandler : IRequestHandler<GetFormAnswersListQuery, IEnumerable<IMapWith<FormAnswers>>>
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetFormAnswersListQueryHandler(ApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    public async Task<IEnumerable<IMapWith<FormAnswers>>> Handle(GetFormAnswersListQuery request, CancellationToken cancellationToken)
+    {
+        var form = await _context.Forms.FindAsync(request.FormId, cancellationToken);
+
+        if (form is null)
+            return Array.Empty<IMapWith<FormAnswers>>();
+
+        return _mapper.ProjectTo<FormAnswersDto>(
+            _context.FormAnswers
+            .Where(fa => fa.FormId == form.Id));
+    }
+}
