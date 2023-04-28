85ebd87766c1dc681fbc4232c96d2f6d6f33f6a6
Katy248 <petrovanton247@gmail.com>
2023-04-28 16:55:44 +0300

    Visual style changes

--- Forms.Mvc/wwwroot/js/site.js
+++ Forms.Mvc/wwwroot/js/site.js
@@ -1,4 +1,39 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+const setTheme = (theme) => {
+    document.body.setAttribute("data-bs-theme", theme);
 
-// Write your JavaScript code.
+    if (theme == 'dark') {
+        document.getElementById('onLightThemeSwitchLabel').hidden = true;
+        document.getElementById('onDarkThemeSwitchLabel').hidden = false;
+    } else {
+        document.getElementById('onLightThemeSwitchLabel').hidden = false;
+        document.getElementById('onDarkThemeSwitchLabel').hidden = true;
+    }
+
+    console.log(`Set ${theme} theme`);
+}
+
+const scheme = window.matchMedia('(prefers-color-scheme: dark)');
+const themeSwitcher = document.getElementById('themeSwitcher');
+
+themeSwitcher.addEventListener('click', event => {
+    localStorage.setItem('theme', themeSwitcher.checked ? 'dark' : 'light');
+    checkTheme();
+});
+
+checkTheme = () => {
+    if (localStorage.getItem('theme')) {
+        const theme = localStorage.getItem('theme')
+        setTheme(theme);
+        themeSwitcher.checked = theme == 'dark';
+        console.log(`Set ${theme} theme from storage`);
+    } else {
+        setTheme(scheme.matches ? 'dark' : 'light');
+
+        scheme.addEventListener('change', event => {
+            setTheme(event.matches ? 'dark' : 'light');
+        });
+    }
+}
+
+checkTheme();
