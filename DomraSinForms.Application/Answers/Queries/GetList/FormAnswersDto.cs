08f55267b00009689d0820c10187fc76e78edd64
Katy248 <petrovanton247@gmail.com>
2023-05-17 14:50:53 +0300

    Add modal to show answers on form

--- DomraSinForms.Application/Answers/Queries/GetList/FormAnswersDto.cs
+++ DomraSinForms.Application/Answers/Queries/GetList/FormAnswersDto.cs
@@ -9,7 +9,6 @@ public class FormAnswersDto : IMapWith<FormAnswers>
     public string Id { get; set; }
     public string FormId { get; set; }
     public string UserId { get; set; }
-    public string CreatorId { get; set; }
     public DateTime CreationDate { get; set; }
     public List<Answer> Answers { get; set; }
     public void Mapping(Profile profile)
