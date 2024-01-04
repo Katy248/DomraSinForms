167c7d0a285bc55187203f4c6638a21eec76a10f
Katy248 <petrovanton247@gmail.com>
2024-01-04 18:22:59 +0300

    Start creating User-Register feature

---
+++ DomraSin.Application/Features/Users/Register/RequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomraSin.Application.Extensions;
+using FluentValidation;
+using MediatR;
+
+namespace DomraSin.Application.Features.Users.Register;
+public class RequestHandler(
+    IEnumerable<IValidator<Request>> validators) : IRequestHandler<Request>
+    
+{
+    private readonly IEnumerable<IValidator<Request>> _validators = validators;
+
+    public async Task Handle(Request request, CancellationToken cancellationToken)
+    {
+        if (!await _validators.ValidateMany(request, cancellationToken))
+            return;
+    }
+}
