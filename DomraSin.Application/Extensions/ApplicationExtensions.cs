28e934b8afb51358112bcdb6c204feeabed87eaf
Katy248 <petrovanton247@gmail.com>
2024-01-04 11:39:35 +0300

    Add extensions methods

---
+++ DomraSin.Application/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DomraSin.Application.Extensions;
+public static class ApplicationExtensions
+{
+    public static IServiceCollection AddApplicationLayer(this IServiceCollection services) =>
+        services;
+}
---
+++ DomraSin.Persistance/Extensions/PersistenceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DomraSin.Persistence.Extensions;
+public static class PersistenceExtensions
+{
+    public static IServiceCollection AddPersistenceLayer(this IServiceCollection services) =>
+        services;
+}
