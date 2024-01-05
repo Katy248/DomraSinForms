a6b5962e700babf4da330758c40dc4ad0f69abe0
ForesLov <fores.clerc@gmail.com>
2024-01-05 20:26:35 +0300

    Add relation and DbSets

---
+++ DomraSin.Persistance/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DomraSin.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DomraSin.Persistence
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<Form> Forms { get; set; }
+        public DbSet<FormAnswers> FormAnswers { get; set; }
+        public DbSet<FormItem> FormItems { get; set; }
+        public DbSet<ScoreType> ScoreTypes{ get; set; }
+    }
+}
