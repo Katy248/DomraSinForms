c68ef4a7a5efd1d9af64d13999b7ad17a4b50e3c
Katy248 <petrovanton247@gmail.com>
2024-01-11 09:48:29 +0300

    Update interface

--- DomraSin.Domain/Interfaces/Repositories/IUsersRepository.cs
+++ DomraSin.Domain/Interfaces/Repositories/IUsersRepository.cs
@@ -2,10 +2,13 @@ using DomraSin.Domain.Models;
 
 namespace DomraSin.Domain.Interfaces.Repositories;
 
-public interface IUsersRepository : ICollectionRepository<User>
+public interface IUsersRepository
 {
-    Task<Form> Get(string id, CancellationToken cancellationToken);
+    Task<User> Get(string id, CancellationToken cancellationToken);
+    Task<User> GetByEmail(string email, CancellationToken cancellationToken);
     //IQueryable<QuestionOption> GetCollection(string questionId);
+
+    Task<bool> UserExists(string email, CancellationToken cancellationToken);
     Task<bool> Insert(User user, CancellationToken cancellationToken);
     Task<bool> Delete(string userId, CancellationToken cancellationToken);
 }
