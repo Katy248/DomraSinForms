73aaeef857e09a0c43fadf7ce45889520d700e77
Katy248 <petrovanton247@gmail.com>
2024-01-11 09:51:29 +0300

    Add password service

--- DomraSin.Application/Extensions/ApplicationExtensions.cs
+++ DomraSin.Application/Extensions/ApplicationExtensions.cs
@@ -4,11 +4,16 @@ using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomraSin.Application.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DomraSin.Application.Extensions;
 public static class ApplicationExtensions
 {
     public static IServiceCollection AddApplicationLayer(this IServiceCollection services) =>
-        services;
+        services.AddMediatR(builder =>
+        {
+
+        })
+        .AddTransient<PasswordService>();
 }
