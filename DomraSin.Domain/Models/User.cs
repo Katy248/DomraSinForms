0bb27329396e4189beeda5afc5ba631ebd6364b2
Katy248 <petrovanton247@gmail.com>
2024-01-11 09:48:11 +0300

    Change user model

--- DomraSin.Domain/Models/User.cs
+++ DomraSin.Domain/Models/User.cs
@@ -3,7 +3,7 @@
 public class User : EntityBase
 {
     public string Name { get; set; }
-    public string Nickname { get; set; }
+    public string? Nickname { get; set; }
     public string Mail { get; set; }
     public bool Verified { get; set; }
     public string PasswordHash { get; set; }
