eaa1e47ffff4d2223fa2aedae5ea74558cde5ac4
Katy248 <petrovanton247@gmail.com>
2024-01-13 15:00:55 +0300

    Add login feature

---
+++ DomraSin.Application/Features/Users/Login/Response.cs
@@ -0,0 +1,6 @@
+﻿namespace DomraSin.Application.Features.Users.Login;
+
+internal record Response(bool IsSuccess, string? JwtToken = null)
+{
+    public static readonly Response Failed = new(false);
+};
