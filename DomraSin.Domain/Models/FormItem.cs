64590994570c257625de5aeebebe7a313c8e52b5
Katy248 <petrovanton247@gmail.com>
2024-02-04 14:44:44 +0300

    Update models

--- DomraSin.Domain/Models/FormItem.cs
+++ DomraSin.Domain/Models/FormItem.cs
@@ -7,11 +7,10 @@ public class FormItem<TId> where TId : struct, FormItemId
 {
     public TId Id { get; set; }
     public int Index { get; set; }
-    public FormItemType Types { get; set; }
+    public string Descriptor { get; set; } => typeof(TId).Name;
+    public bool Is<TFormItem, TId2>() where TFormItem : new(), FormItem<TId2> where TId2 : FormItemId
+    {
+        return this.Descriptor == typeof(TId2).Name;
+    }
 }
 public readonly record struct FormItemId(Guid Value);
-
-public enum FormItemType
-{
-    Question, Picture
-}
--- DomraSin.Domain/Models/FormItems/PictureItem.cs
+++ DomraSin.Domain/Models/FormItems/PictureItem.cs
@@ -2,7 +2,6 @@
 
 public class PictureItem : FormItem<PictureItemId>
 {
-    public PictureItem() => this.Type = FormItemType.Question;
     public string PictureLink { get; set; }
     public string AltText { get; set; }
 }
