4781d9df892163cfcba3704fbc20e39164ad3290
Katy248 <petrovanton247@gmail.com>
2024-02-04 15:14:29 +0300

    Update repository base

--- DomraSin.Persistance/Repositories/RepositoryBase.cs
+++ DomraSin.Persistance/Repositories/RepositoryBase.cs
@@ -1,12 +1,30 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
+using DomraSin.Persistence;
 
 namespace DomraSin.Persistance;
 
 public abstract class RepositoryBase<TEntity, TId> where TId : struct
 {
-    public Task<bool> Delete(TId id, CancellationToken cancellationToken)
+    private readonly ApplicationDbContext _context;
+    private readonly ILogger _logger;
+
+    public RepositoryBase(ApplicationDbContext context, ILogger logger)
     {
-        
+        _context = context;
+        _logger = logger;
+    }
+    public virtual async Task BeforeDelete(TId id, CancellationToken cancellationToken)
+    {
+        _logger.LogDebug($"No before delete action for {typeof(TEntity).Name}");
+    }
+    public virtual async Task<bool> Delete(TId id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
     } 
+    public virtual Task<bool> Insert(TEntity entity, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
