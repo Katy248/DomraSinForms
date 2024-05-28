0ac501346a789a63536a093754c684a50ea0da0a
Katy248 <petrovanton247@gmail.com>
2024-05-28 12:39:03 +0300

    Updated AuthStateProvider

--- DomraSinForms.Client/AppAuthinticationStateProvider.cs
+++ DomraSinForms.Client/AppAuthinticationStateProvider.cs
@@ -1,11 +1,31 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace DomraSinForms.Client;
 
 public class AppAuthinticationStateProvider : AuthenticationStateProvider
 {
-    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    private const string AuthTokenKey = "AUTH_TOKEN";
+    private static readonly AuthenticationState NotAuthorized = new(new(new ClaimsIdentity()));
+    private readonly ProtectedLocalStorage _localStorage;
+
+    public AppAuthinticationStateProvider(ProtectedLocalStorage localStorage)
     {
-        throw new NotImplementedException();
+        _localStorage = localStorage;
+    }
+    
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var token = await _localStorage.GetAsync<string>(AuthTokenKey);
+        if (token.Value is null)
+            return NotAuthorized;
+        
+        JwtSecurityTokenHandler handler = new();
+        var jwt = handler.ReadJwtToken(token.Value);
+
+        return new AuthenticationState(new(new ClaimsIdentity(jwt.Claims)));
     }
 }
